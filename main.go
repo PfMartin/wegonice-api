77008d4899e328d37a37d96c7be049ae971d9cf5
PfMartin <martinpfatrisch@gmail.com>
2024-03-07 20:18:03 +0100

    Added build and static check to ci

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/PfMartin/wegonice-api/config"
@@ -15,6 +16,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	s := fmt.Sprintf("HEllo")
+	fmt.Println(s)
 
 	_, cancel := db.NewDatabase(conf.DBName, conf.DBUser, conf.DBPassword, conf.DBURI)
 	defer cancel()

b1290f0a744f7c858b863452857b93c71323bc85
PfMartin <martinpfatrisch@gmail.com>
2024-03-09 08:09:25 +0100

    Added password functions and test

---
+++ util/password_test.go
@@ -0,0 +1,46 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestPassword(t *testing.T) {
+	testCases := []struct {
+		name      string
+		password1 string
+		password2 string
+		isEqual   bool
+	}{
+		{
+			name:      "Success with the same password",
+			password1: "testpassword",
+			password2: "testpassword",
+			isEqual:   true,
+		},
+		{
+			name:      "Fail with different passwords",
+			password1: "test",
+			password2: "password",
+			isEqual:   false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			hashedPassword1, err := HashPassword(tc.password1)
+			require.NoError(t, err)
+			require.NotEmpty(t, hashedPassword1)
+
+			err = CheckPassword(tc.password2, hashedPassword1)
+			if tc.isEqual {
+				require.NoError(t, err)
+			} else {
+				require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+			}
+		})
+
+	}
+}

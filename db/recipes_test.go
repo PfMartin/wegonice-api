0dfd34466d0f1236b576e5bdc71b10a93d4cae07
PfMartin <martinpfatrisch@gmail.com>
2024-03-16 21:27:19 +0100

    Added print statement

--- db/recipes_test.go
+++ db/recipes_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"sync"
 	"testing"
 	"time"
@@ -154,6 +155,10 @@ func TestUnitGetAllRecipes(t *testing.T) {
 		recipes, err := recipeColl.GetAllRecipes(ctx, pagination)
 		require.NoError(t, err)
 		require.NotEmpty(t, recipes)
+
+		for _, r := range recipes {
+			fmt.Println(r.Author)
+		}
 
 		require.Equal(t, int(pagination.PageSize), len(recipes))
 
